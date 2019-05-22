1f0c98a6b114d3c92b8cf973cfe71ede6f928c0e
wanwufusu <760931928@qq.com>
2019-05-22 14:29:14 +0800

    邹睿明：springboot框架

---
+++ mall/src/main/java/com/springboot/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Ruimingz
+ * @create 2019/5/22
+ * @since 1.0.0
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("hello")
+    public String testHello(){
+        return "hello";
+    }
+}
