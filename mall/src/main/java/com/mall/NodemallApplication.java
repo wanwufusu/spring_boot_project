df8706b9d5d4dffed7057873a4c79a6f98a9520b
wanwufusu <760931928@qq.com>
2019-05-22 11:59:11 +0800

    邹睿明：springboot框架

---
+++ mall/src/main/java/com/mall/NodemallApplication.java
@@ -0,0 +1,13 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodemallApplication.class, args);
+    }
+
+}
