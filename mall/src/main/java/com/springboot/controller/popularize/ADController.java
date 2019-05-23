1c1fdc7abd6b3c0adef1506a7f9f956d31314e09
YUTIN <827118859@qq.com>
2019-05-23 22:53:59 +0800

    杨渝挺：不完善的添加，(没有顺带返回插入的对象完整信息)

--- mall/src/main/java/com/springboot/controller/popularize/ADController.java
+++ mall/src/main/java/com/springboot/controller/popularize/ADController.java
@@ -1,10 +1,12 @@
 package com.springboot.controller.popularize;
 
+import com.springboot.bean.popularize.MallAd;
 import com.springboot.bean.util.PageDetail;
 import com.springboot.bean.util.ResponseVO;
 import com.springboot.service.popularize.ADService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,12 +17,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ADController {
 
     @Autowired
-    ADService adService;
+    ADService service;
 
     @RequestMapping("list")
     @ResponseBody
     public ResponseVO adList(PageDetail pageDetail){
-        ResponseVO response = new ResponseVO(adService.findList(pageDetail), "内容", 0);
+        ResponseVO response = new ResponseVO(service.findList(pageDetail), "内容", 0);
         return response;
     }
+
+    @RequestMapping("create")
+    @ResponseBody
+    public ResponseVO create (@RequestBody MallAd ad){
+        ResponseVO<Object> vo = new ResponseVO<>();
+        int i = service.add(ad);
+        if (i>=1){
+            vo.setSuccessMsg();
+            vo.setData(i);
+            return vo;
+        }else {
+            return vo;
+        }
+
+    }
+
 }
--- mall/src/main/java/com/springboot/service/popularize/ADService.java
+++ mall/src/main/java/com/springboot/service/popularize/ADService.java
@@ -1,9 +1,12 @@
 package com.springboot.service.popularize;
 
+import com.springboot.bean.popularize.MallAd;
 import com.springboot.bean.util.PageDetail;
 import com.springboot.bean.util.Result;
 
 public interface ADService {
 
     Result findList(PageDetail pageDetail);
+
+    int add(MallAd ad);
 }
