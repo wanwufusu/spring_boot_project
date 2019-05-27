f782b92f0e327a62e74c7e59e446084f6805c8ad
YUTIN <827118859@qq.com>
2019-05-27 15:47:21 +0800

    杨渝挺： MallConfig

--- mall/src/main/java/com/springboot/controller/MallSystemConfigController.java
+++ mall/src/main/java/com/springboot/controller/MallSystemConfigController.java
@@ -1,11 +1,15 @@
 package com.springboot.controller;
 
 
+import com.springboot.bean.MallSystem;
 import com.springboot.bean.util.ResponseVO;
 import com.springboot.service.MallConfigService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 
 @RestController
@@ -13,24 +17,34 @@ public class MallSystemConfigController {
 
     @Autowired
     MallConfigService service;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping("admin/config/mall")
-    public ResponseVO mall() {
+    /**
+     * 4个配置通用（which无实际意义，仅用于接收），返回ResponseVO，Data里面装KEY-VALUE(来自于数据库的key_name,key_value两个Column)
+     *
+     * @param which
+     * @return
+     */
+    @RequestMapping(value = "config/{which}", method = RequestMethod.GET)
+    public ResponseVO mallGet(@PathVariable("which") String which) {
+
         return null;
     }
 
-    @RequestMapping("admin/config/express")
-    public ResponseVO express() {
+
+    //——————————————————————————————————————————————————————————————————
+
+    /**
+     * 前台接收KEY-VALUE的信息，4个配置通用，返回ResponseVO，Data里面装KEY-VALUE
+     *
+     * @param map
+     * @param which
+     * @return
+     */
+    @RequestMapping(value = "config/{which}", method = RequestMethod.POST)
+    public ResponseVO mallPost(@RequestBody HashMap<String, String> map, @PathVariable("which") String which) {
+
         return null;
     }
 
-    @RequestMapping("admin/config/order")
-    public ResponseVO order() {
-        return null;
-    }
-
-    @RequestMapping("admin/config/wx")
-    public ResponseVO wx() {
-        return null;
-    }
 }
--- mall/src/main/java/com/springboot/service/MallConfigService.java
+++ mall/src/main/java/com/springboot/service/MallConfigService.java
@@ -1,10 +1,10 @@
 package com.springboot.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MallConfigService {
 
+public interface MallConfigService {
 
 }
